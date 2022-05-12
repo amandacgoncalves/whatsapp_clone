831228a6e256281ce76069be30cc614fece9be17
amandacgoncalves <102832741+amandacgoncalves@users.noreply.github.com>
2022-05-12 09:33:32 -0300

    message status

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -189,19 +189,35 @@ export class WhatsAppController {
                     let data = doc.data();
                     data.id = doc.id;
 
-                    if (!this.el.panelMessagesContainer.querySelector('#_'+ data.id)) {
-
                     let message = new Message();
 
                     message.fromJSON(data);
 
                     let me = (data.from === this._user.email)
 
+                    if (!this.el.panelMessagesContainer.querySelector('#_'+ data.id)) {
+
+                    if (!me) {
+
+                        doc.ref.set({
+                            status: 'read'
+                        }, {
+                            merge:true
+                        });//merge
+
+                    }//if not me
+
                     let view = message.getViewElement(me);
 
                     this.el.panelMessagesContainer.appendChild(view);
                     
-                    }//if
+                    } else if (me) {
+
+                    let msgEl = this.el.panelMessagesContainer.querySelector('#_'+ data.id);
+
+                    msgEl.querySelector('.message-status').innerHTML = message.getStatusViewElement().outerHTML;
+
+                    }
 
                 });//docs for each
 
