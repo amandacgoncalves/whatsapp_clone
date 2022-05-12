f2dcea5f233c08eacb279bfae976c3fee5f85205
amandacgoncalves <102832741+amandacgoncalves@users.noreply.github.com>
2022-05-12 10:46:32 -0300

    sending gallery files

--- src/util/Firebase.js
+++ src/util/Firebase.js
@@ -9,7 +9,7 @@ export class Firebase {
             apiKey: "AIzaSyAXEmal-ug5Bo4zN-nMiBljwxCmTIWtec4",       
             authDomain: "whatsapp-clone-d41d6.firebaseapp.com",        
             projectId: "whatsapp-clone-d41d6",        
-            storageBucket: "whatsapp-clone-d41d6.appspot.com",      
+            storageBucket: "gs://whatsapp-clone-d41d6.appspot.com",      
             messagingSenderId: "583583946958",
             appId: "1:583583946958:web:7016afe4a479c0b013189f",
             measurementId: "G-GD018CPVY9"
